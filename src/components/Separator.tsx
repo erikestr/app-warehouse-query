bf77b98dd49cd2d44dc227a68a427ffe5cac9fb9
erik.estrada <erik.estrada@jayor.com.mx>
2024-01-11 17:01:14 -0600

    .gitignore is now working

--- src/components/Separator.tsx
+++
@@ -1,3 +0,0 @@
-export const Separator = () => {
-    return <hr className="border-t-2 border-dashed border-gray-500 mb-4" />;
-};
--- src/types/EsCardHeaderProps.tsx
+++
@@ -1,5 +0,0 @@
-export interface EsCardHeaderProps {
-    ITEMNMBR: string;
-    ITEMDESC: string;
-    DEF01STR?: string; // Optional parameter
-}
