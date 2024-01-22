c4c0b0eefb5cf21707d4367becc47dd74eab8b6f
erikes <erikestrada37@outlook.com>
2024-01-22 12:08:46 -0600

    v1.0.240122.r.0001 [font-change] Change on font size of the Cards

--- src/components/EsCard.tsx
+++ src/components/EsCard.tsx
@@ -9,30 +9,30 @@ export const EsCard = ({ ITEMNMBR, ITEMDESC, LOTNUMBR, BIN, EAN13Adaia,
 
             <div className='flex flex-row justify-center'>
                 <div className='flex flex-col'>
-                    <p className='mt-2 text-2xl'>Lote</p>
-                    <p className='mt-0 text-xl text font-bold'>{LOTNUMBR}</p>
+                    <p className='mt-2 text-xl'>Lote</p>
+                    <p className='mt-0 text font-bold'>{LOTNUMBR}</p>
                 </div>
             </div>
             <div className='flex flex-row'>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Fecha Fab.</p>
-                    <p className='mt-0 text-xl text font-bold'>{format(MFGDATE, 'dd-MM-yyyy')}</p>
+                    <p className='mt-2 text-xl'>Fecha Fab.</p>
+                    <p className='mt-0  text font-bold'>{format(MFGDATE, 'dd-MM-yyyy')}</p>
                 </div>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Fecha Cad.</p>
-                    <p className='mt-0 text-xl text font-bold'>{format(EXPNDATE, 'dd-MM-yyyy')}</p>
+                    <p className='mt-2 text-xl'>Fecha Cad.</p>
+                    <p className='mt-0  text font-bold'>{format(EXPNDATE, 'dd-MM-yyyy')}</p>
                 </div>
 
             </div>
 
             <div className='flex flex-row'>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Existencia</p>
-                    <p className='mt-0 text-xl text font-bold'>{QTYONHND > 0 ? QTYONHND?.toLocaleString() : '-'}</p>
+                    <p className='mt-2 text-xl'>Existencia</p>
+                    <p className='mt-0  text font-bold'>{QTYONHND > 0 ? QTYONHND?.toLocaleString() : '-'}</p>
                 </div>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Disponible</p>
-                    <p className='mt-0 text-xl text font-bold'>{QTYDISPO > 0 ? QTYDISPO.toLocaleString() : '-'}</p>
+                    <p className='mt-2 text-xl'>Disponible</p>
+                    <p className='mt-0  text font-bold'>{QTYDISPO > 0 ? QTYDISPO.toLocaleString() : '-'}</p>
                 </div>
             </div>
 
--- src/components/EsCardByBatch.tsx
+++ src/components/EsCardByBatch.tsx
@@ -8,42 +8,42 @@ const EsCardByBatch = ({ ITEMNMBR, ITEMDESC, DEF01STR, LOTNUMBR, BIN, EAN13Adaia
 
             <div className='flex flex-row'>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Artículo</p>
-                    <p className='mt-0 text-xl font-bold'>{ITEMNMBR}</p>
+                    <p className='mt-2 text-xl'>Artículo</p>
+                    <p className='mt-0  font-bold'>{ITEMNMBR}</p>
                 </div>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Registro Sanitario</p>
-                    <p className='mt-0 text-xl font-bold'>{DEF01STR}</p>
-                </div>
-            </div>
-
-            <div className='flex flex-row'>
-                <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Descripción</p>
-                    <p className='mt-0 text-xl font-bold'>{ITEMDESC}</p>
+                    <p className='mt-2 text-xl'>Registro Sanitario</p>
+                    <p className='mt-0  font-bold'>{DEF01STR}</p>
                 </div>
             </div>
 
             <div className='flex flex-row'>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Fecha Fab.</p>
-                    <p className='mt-0 text-xl font-bold'>{format(MFGDATE, 'dd-MM-yyyy')}</p>
+                    <p className='mt-2 text-xl'>Descripción</p>
+                    <p className='mt-0  font-bold'>{ITEMDESC}</p>
+                </div>
+            </div>
+
+            <div className='flex flex-row'>
+                <div className='flex flex-col w-full'>
+                    <p className='mt-2 text-xl'>Fecha Fab.</p>
+                    <p className='mt-0  font-bold'>{format(MFGDATE, 'dd-MM-yyyy')}</p>
                 </div>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Fecha Cad.</p>
-                    <p className='mt-0 text-xl font-bold'>{format(EXPNDATE, 'dd-MM-yyyy')}</p>
+                    <p className='mt-2 text-xl'>Fecha Cad.</p>
+                    <p className='mt-0  font-bold'>{format(EXPNDATE, 'dd-MM-yyyy')}</p>
                 </div>
 
             </div>
 
             <div className='flex flex-row'>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Existencia</p>
-                    <p className='mt-0 text-xl font-bold'>{QTYONHND > 0 ? QTYONHND.toLocaleString() : '-'}</p>
+                    <p className='mt-2 text-xl'>Existencia</p>
+                    <p className='mt-0  font-bold'>{QTYONHND > 0 ? QTYONHND.toLocaleString() : '-'}</p>
                 </div>
                 <div className='flex flex-col w-full'>
-                    <p className='mt-2 text-2xl'>Disponible</p>
-                    <p className='mt-0 text-xl font-bold'>{QTYDISPO > 0 ? QTYDISPO.toLocaleString() : '-'}</p>
+                    <p className='mt-2 text-xl'>Disponible</p>
+                    <p className='mt-0  font-bold'>{QTYDISPO > 0 ? QTYDISPO.toLocaleString() : '-'}</p>
                 </div>
             </div>
 
